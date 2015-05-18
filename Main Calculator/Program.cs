665b2cb0ba035e19981ec4ad427d0d33988f26a3
Keizar <matrixworlds@gmail.com>
2015-05-18 22:39:07 +0300

    Added version 0.1

---
+++ Main Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter the digits and operations:");
+        string[] input = Console.ReadLine().Split(' ');
+        int firstNumber = int.Parse(input[0]);
+        int secondNumber = int.Parse(input[2]);
+        int result = 0;
+
+        while (input[0] != "End")
+        {
+            switch (input[1])
+            {
+                case "+":
+                {
+                    result = firstNumber + secondNumber;
+                    break;
+                }
+                case "-":
+                {
+                    result = firstNumber - secondNumber;
+                    break;
+                }
+
+            }
+            Console.WriteLine(result);
+            input = Console.ReadLine().Split(' ');
+        }
+    }
+}
+
